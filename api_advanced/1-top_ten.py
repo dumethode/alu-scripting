0099846c0cd5c9dedb0e9de225f8b999c5eaea28
dumethode <m.duhujubum@alustudent.com>
2025-10-26 02:47:48 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,43 +1,17 @@
 #!/usr/bin/python3
-"""
-Module that queries the Reddit API and prints the titles of the first
-10 hot posts for a given subreddit
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    Args:
-        subreddit: name of the subreddit
-
-    Returns:
-        None (prints titles or None if invalid)
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-        return
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        'User-Agent': 'linux:api_advanced:v1.0.0 (by /u/yourusername)'
-    }
-    params = {'limit': 10}
-
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            
-            for post in posts:
-                print(post.get('data', {}).get('title'))
-        else:
-            print("None")
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
-        print("None")
+        print(None)
