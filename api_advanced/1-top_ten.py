dc43c8e18bb54d08fcec997f54915130b6246ac3
dumethode <m.duhujubum@alustudent.com>
2025-10-26 02:50:57 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+""" Function that queries the Reddit API and prints the titles
+of the first 10 hot posts for a given subreddit.
+"""
 import requests
 
+
 def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts of a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'MethodeBot/0.1'}
+    headers = {"User-Agent": "MethodeBot/0.1"}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-        data = response.json().get('data', {}).get('children', [])
-        if not data:
-            print(None)
-            return
-
-        for post in data:
-            print(post['data']['title'])
-
-    except Exception:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+    if not data:
+        print(None)
+        return
+
+    for post in data:
+        print(post["data"]["title"])
 
