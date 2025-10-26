1e29c0e2861ab67361258e4a8ec5d4a9689c7bf9
dumethode <m.duhujubum@alustudent.com>
2025-10-26 02:25:37 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -10,19 +10,19 @@ def top_ten(subreddit):
     """
     Queries the Reddit API and prints the titles of the first 10 hot posts
     for a given subreddit.
-    
+
     Args:
         subreddit: name of the subreddit
-        
+
     Returns:
         None (prints titles or None if invalid)
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-               'AppleWebKit/537.36 (KHTML, like Gecko) '
-               'Chrome/91.0.4472.124 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
     params = {'limit': 10}
-    
+
     try:
         response = requests.get(url, headers=headers, params=params,
                                 allow_redirects=False)
